c16f267e0399b9188cda9ed7a190e65d2254c3a2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-11 09:51:00 -0700

    docs

---
+++ doc.go
@@ -0,0 +1,5 @@
+/*
+Package memdigest provides an in memory content-addressable storage (CAS),
+and can be used as a CAS for the digestfs (https://github.com/reiver/go-digestfs) content-addressable virtual file system (VFS).
+*/
+package memdigest
